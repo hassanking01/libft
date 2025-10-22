9625be38fdb0370bdad4b0b8e6489331766f74f8
Hasan Ahchtar <hahchtar@c1r3p10.1337.ma>
2025-10-22 14:02:46 +0100

    fix some functions

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: hahchtar <hahchtar@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 22:55:22 by hahchtar          #+#    #+#             */
-/*   Updated: 2025/10/21 16:56:44 by hahchtar         ###   ########.fr       */
+/*   Updated: 2025/10/22 12:42:16 by hahchtar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
@@ -35,6 +35,8 @@ size_t	count_words(const char *s, char c)
 	size_t	i;
 	size_t	count;
 
+	if (!c)
+		return (1);
 	count = 0;
 	i = 0;
 	is_added = 0;
@@ -61,7 +63,6 @@ void	fill_array(char **ptr, const char *s, char c)
 	size_t	ptr_i;
 	size_t	count_n;
 
-	count_n = 0;
 	ptr_i = 0;
 	i = 0;
 	while (s[i] == c)
@@ -71,14 +72,16 @@ void	fill_array(char **ptr, const char *s, char c)
 		if (s[i] != c)
 		{
 			count_n = 0;
-			while (s[i + count_n] != c)
+			while ((s[i + count_n] != c) && s[i + count_n])
 			{
 				count_n++;
 			}
-			ptr[ptr_i++] = ft_strndup(&s[i], count_n);
+			ptr[ptr_i] = ft_strndup(&s[i], count_n);
 			i += count_n;
+			ptr_i++;
 		}
-		i++;
+		else
+			i++;
 	}
 	ptr[ptr_i] = NULL;
 }
@@ -89,9 +92,17 @@ char	**ft_split(char const *s, char c)
 	size_t	i;
 	size_t	count;
 
+	if (!(*s))
+	{
+		ptr = malloc(sizeof(char *));
+		ptr[0] = NULL;
+		return (ptr);
+	}
 	i = 0;
 	count = count_words(s, c);
 	ptr = malloc((count + 1) * sizeof(char *));
+	if (!ptr)
+		return (NULL);
 	fill_array(ptr, s, c);
 	return (ptr);
 }
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,10 +6,12 @@
 /*   By: hahchtar <hahchtar@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 22:18:31 by hahchtar          #+#    #+#             */
-/*   Updated: 2025/10/19 18:01:31 by hahchtar         ###   ########.fr       */
+/*   Updated: 2025/10/22 12:09:13 by hahchtar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
+#include <stddef.h>
+#include <stdlib.h>
 
 static int	is_from_set(char const *set, char check)
 {
@@ -25,21 +27,39 @@ static int	is_from_set(char const *set, char check)
 	return (0);
 }
 
-char	*ft_strtrim(char const *s1, char const *set)
+static char	*ft_strndup(char const *s1, size_t n)
 {
-	char	*dup;
 	char	*ptr;
 	size_t	i;
 
+	i = 0;
+	ptr = malloc(n + 1);
+	while (i < n)
+	{
+		ptr[i] = s1[i];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*ptr;
+	size_t	i;
+	size_t	j;
+	size_t	s_len;
+
+	s_len = ft_strlen(s1);
+	j = 0;
 	if (!s1 || !set)
 		return (NULL);
-	i = ft_strlen(s1) - 1;
-	dup = ft_strdup(s1);
-	while (is_from_set(set, dup[i]))
-		dup[i--] = '\0';
-	i = 0;
-	while (is_from_set(set, dup[i]))
-		i++;
-	ptr = ft_strdup(&dup[i]);
+	j = 0;
+	while (j < s_len && is_from_set(set, s1[j]))
+		j++;
+	i = s_len;
+	while (i > j && is_from_set(set, s1[i -1]))
+		i--;
+	ptr = ft_strndup(&s1[j], i - j);
 	return (ptr);
 }
